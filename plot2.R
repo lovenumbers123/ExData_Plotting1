4139a8df15eb91e4040a36b1ac2a279cde4c4dfb
lovenumbers123 <lovenumbers123@yahoo.com>
2016-11-20 00:24:01 -0600

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,63 @@
+
+R version 3.3.1 (2016-06-21) -- "Bug in Your Hair"
+Copyright (C) 2016 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[Previously saved workspace restored]
+
+> ## Plot 2
+> ## downloaded and unzipped necessary file
+>  if(!file.exists("./data")){dir.create("./data")}
+>  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+>  download.file(fileUrl,destfile="./data/Dataset.zip",method="auto")
+trying URL 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+Content type 'application/zip' length 20640916 bytes (19.7 MB)
+downloaded 19.7 MB
+
+>  unzip(zipfile="./data/Dataset.zip",exdir="./data")
+> 
+> ## constructed path for files
+>  wearpath <- file.path(C:\Users\Denise Laptop\Documents\data\household_power_consumption.txt)
+Error: unexpected input in " wearpath <- file.path(C:\"
+> 
+> ## variable to list the files
+>  files <-list.files(wearpath,recursive=TRUE)
+Error in list.files(wearpath, recursive = TRUE) : 
+  object 'wearpath' not found
+> 
+> ## Set working Directory
+> setwd("~/data/")
+> 
+> ## reading files
+> globalpower <- read.table("household_power_consumption.txt",skip=1,sep=";")
+> 
+> ## assigning  names
+> names(globalpower) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+> 
+> ## assigning  2007 subset data
+> globalpowerset <- subset(globalpower,globalpower$Date=="1/2/2007" | globalpower$Date =="2/2/2007")
+> 
+> ## Converting the date and time characters into objects
+> globalpowerset$Date <- as.Date(globalpowerset$Date, format="%d/%m/%Y")
+> globalpowerset$Time <- strptime(globalpowerset$Time, format="%H:%M:%S")
+> globalpowerset[1:1440,"Time"] <- format(globalpowerset[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+> globalpowerset[1441:2880,"Time"] <- format(globalpowerset[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+> 
+> ## making plot
+> plot(globalpowerset$Time,as.numeric(as.character(globalpowerset$Global_active_power)),type="l",main="Global Active Power Vs Time",xlab="",ylab="Global Active Power (kilowatts)") 
+> 
+> 
